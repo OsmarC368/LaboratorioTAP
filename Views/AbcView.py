78ffab6df88dd356d0c5940bf1aea22585c047d1
Osmar Cuicas <Osmargabriel10@gmail.com>
2025-04-23 22:13:25 -0400

    ns

--- Views/AbcView.py
+++ Views/AbcView.py
@@ -2,6 +2,26 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
+dataDefault = [["a", 80, 522],
+               ["b", 514, 54.07],
+               ["c", 19, 0.65],
+               ["d", 2442, 16.11],
+               ["e", 650, 4.61],
+               ["f", 128, 0.63],
+               ["g", 2500, 1.2],
+               ["h", 4, 22.05],
+               ["i", 25, 5.01],
+               ["j", 2232, 2.48],
+               ["k", 2, 4.78],
+               ["l", 1, 38.03],
+               ["m", , ],
+               ["n", , ],
+               ["o", , ],
+               ["p", , ],
+               ["q", , ],
+               ["r", , ],
+               ["s", , ],
+               ]
 
 class AbcView:
     def __init__(self, controller):
@@ -12,8 +32,11 @@ class AbcView:
         self.window.resizable(0,0)
         self.window.configure(background="#2b2f36")
         #labels
-        self.labelTitle = tk.Label(self.window, text="\-Menu Principal-/", font=("Arial", 14, "bold"), foreground="WHITE", background="#2b2f36")
+        self.labelTitle = tk.Label(self.window, text="\-Determinar Tipo ABC-/", font=("Arial", 14, "bold"), foreground="WHITE", background="#2b2f36")
         self.labelTitle.pack(pady=8)
+
+        #TreeView
+        self.tabla = ttk.Treeview()
 
     def show(self):
         self.window.mainloop()
